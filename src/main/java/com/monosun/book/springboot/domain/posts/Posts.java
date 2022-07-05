5f8608b0a64d413fc680c58cfeb06471be9f19fb
jini <javaatoz@gmail.com>
2022-07-05 20:51:19 +0900

    Added Spring Data JPA Test Code

---
+++ src/main/java/com/monosun/book/springboot/domain/posts/Posts.java
@@ -0,0 +1,35 @@
+package com.monosun.book.springboot.domain.posts;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+//lombook
+@Getter
+@NoArgsConstructor
+//JPA
+@Entity  //posts table
+public class Posts {
+
+    @Id  //PK
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //PK Rule auto_increment
+    private Long id;
+
+    @Column(length=500,nullable = false)//VARCHAR(500) not null
+    private String title;
+
+    @Column(columnDefinition = "TEXT",nullable = false)//TEXT type not null
+    private String content;
+
+    private String author;
+
+    @Builder
+    public Posts(String title, String content, String author){
+        this.title =title;
+        this.content=content;
+        this.author=author;
+    }
+
+}
