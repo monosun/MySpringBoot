09085617b5ddf92bb2cee6493acab815db9f390c
monosun <javaatoz@gmail.com>
2022-07-13 00:51:16 +0900

    게시판 작성 완료 및 SpringBoot 2.6.9 버전으로 다운 한글 문제로..

--- src/main/java/com/monosun/book/springboot/service/posts/PostsService.java
+++ src/main/java/com/monosun/book/springboot/service/posts/PostsService.java
@@ -2,13 +2,17 @@ package com.monosun.book.springboot.service.posts;
 
 import com.monosun.book.springboot.domain.posts.Posts;
 import com.monosun.book.springboot.domain.posts.PostsRepository;
+import com.monosun.book.springboot.web.dto.PostsListResponseDto;
 import com.monosun.book.springboot.web.dto.PostsResponseDto;
 import com.monosun.book.springboot.web.dto.PostsSaveRequestDto;
 import com.monosun.book.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import javax.transaction.Transactional;
+import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -29,5 +33,18 @@ public class PostsService {
     public PostsResponseDto findById(Long id){
         Posts entity=postsRepository.findById(id).orElseThrow(()->new IllegalArgumentException("해당 게시글이 없습니다. id="+id));
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc(){
+        return postsRepository.findAllDesc().stream().map(PostsListResponseDto::new).collect(Collectors.toList());
+
+    }
+
+    @Transactional
+    public void delete(Long id){
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(()-> new IllegalArgumentException("해당 게시글이 없습니다. id="+id));
+        postsRepository.delete(posts);
     }
 }
