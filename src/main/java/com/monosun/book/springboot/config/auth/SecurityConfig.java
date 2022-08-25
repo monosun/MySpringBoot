9a2fe205d12c3558f5e330ea9eee54eb7dc9311c
jini <javaatoz@gmail.com>
2022-08-25 20:34:01 +0900

    db jdbc configuration

--- src/main/java/com/monosun/book/springboot/config/auth/SecurityConfig.java
+++ src/main/java/com/monosun/book/springboot/config/auth/SecurityConfig.java
@@ -11,14 +11,33 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     private final CustomOAuth2UserService customOAuth2UserService;
 
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception{
+//        http.csrf().disable().headers().frameOptions().disable().and()
+//                .authorizeRequests().antMatchers("/","/css/**","/images/**","/js/**","/h2-console/**").permitAll()
+//                .antMatchers("/api/v1/**").hasRole(Role.USER.name())
+//                .anyRequest().authenticated() //인증
+//                .and().logout().logoutSuccessUrl("/")
+//                .and().oauth2Login().userInfoEndpoint().userService(customOAuth2UserService);
+//    }
+
     @Override
-    protected void configure(HttpSecurity http) throws Exception{
-        http.csrf().disable().headers().frameOptions().disable().and()
-                .authorizeRequests().antMatchers("/","/css/**","/images/**","/js/**","/h2-console/**").permitAll()
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .headers().frameOptions().disable()
+                .and()
+                .authorizeRequests()
+                .antMatchers("/", "/css/**", "/images/**", "/js/**", "/h2-console/**", "/profile").permitAll()
                 .antMatchers("/api/v1/**").hasRole(Role.USER.name())
-                .anyRequest().authenticated() //인증
-                .and().logout().logoutSuccessUrl("/")
-                .and().oauth2Login().userInfoEndpoint().userService(customOAuth2UserService);
+                .anyRequest().authenticated()
+                .and()
+                .logout()
+                .logoutSuccessUrl("/")
+                .and()
+                .oauth2Login()
+                .userInfoEndpoint()
+                .userService(customOAuth2UserService);
     }
 
 
