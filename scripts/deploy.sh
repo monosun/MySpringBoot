ac7a7102dab9d0d7c74f3a3c369e2aade86e2db6
jini <javaatoz@gmail.com>
2022-09-01 19:20:48 +0900

    deploy.sh 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -11,7 +11,7 @@ echo "> 현재 구동 중인 애플리케이션 pid 확인"
 
 CURRENT_PID=$(pgrep -f ${PROJECT_NAME}.*.jar)
 
-echo "현재 구동 중인 애플리케이션 pid: $CURRENT_PID"
+echo "> 현재 구동 중인 애플리케이션 pid: $CURRENT_PID"
 
 if [ -z "$CURRENT_PID" ]; then
         echo "> 현재 구동 중인 애플리케이션이 없으므로 종료하지 않습니다."
@@ -28,8 +28,8 @@ JAR_NAME=$(ls -tr $REPOSITORY/ |grep jar|tail -n 1)
 
 echo "> JAR Name : $JAR_NAME"
 
-echo "> ${JAR_NAME}에 실행권한 추가"
-chmod +x ${JAR_NAME}
+echo "> ${REPOSITORY}/${JAR_NAME}에 실행권한 추가"
+chmod +x ${REPOSITORY}/${JAR_NAME}
 
 echo "> ${JAR_NAME} 실행"
 
