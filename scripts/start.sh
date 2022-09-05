3b4d38b3b3e57216cc1d6287a5b1c2bc413bf542
jini <javaatoz@gmail.com>
2022-09-05 17:14:08 +0900

    version auto 버전 구별 날짜 값반영

--- scripts/start.sh
+++ scripts/start.sh
@@ -11,7 +11,6 @@ echo "> Build 파일 복사"
 echo "> cp $REPOSITORY/zip/*.jar $REPOSITORY/"
 
 cp $REPOSITORY/zip/*.jar $REPOSITORY/
-
 echo "> 새 어플리케이션 배포"
 JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
 
